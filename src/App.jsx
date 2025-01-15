def9bb1e15c05544c24feffa6d2be6b22aa289f1
Latiful <latifulbari2001@gmail.com>
2025-01-16 00:46:22 +0600

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Navbar from "./Components/Navbar";
 import NewsBoard from "./Components/NewsBoard";
+import Footer from "./Components/Footer";
 
 const App = () => {
   const [category, setCategory] = useState("general");
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <Navbar setCategory={setCategory} />
       <NewsBoard category={category} />
+      <Footer />
     </div>
   );
 };
